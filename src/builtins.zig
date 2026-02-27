56aecf738c8be3af8b12a5deb261856551d775d9
Tom Huntington <thomasp.huntington@outlook.com>
2026-02-28 12:34:47 +1300

    add fn for scalars

--- src/builtins.zig
+++ src/builtins.zig
@@ -4,11 +4,20 @@ const Value = types.Value;
 
 pub fn add(all: std.mem.Allocator, a: Value, b: Value) Value {
     _ = all;
-    switch (.{ a, b }) {
-        .{ Value.scalar, Value.scalar } => {},
+
+    switch (a) {
+        .scalar => |as| {
+            switch (b) {
+                .scalar => |bs| {
+                    const val = as.value + bs.value;
+                    return .{ .scalar = .{ .value = val, .is_char = bs.is_char and as.is_char } };
+                },
+                else => {},
+            }
+        },
         else => {},
     }
-    return b;
+    @panic("not implemented");
 }
 pub fn mul(all: std.mem.Allocator, a: Value, b: Value) Value {
     _ = a;
